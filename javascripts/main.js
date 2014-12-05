701da87fcad7c55c1f76da631cc10ac9500b0979
Hasitha Prageeth <haztha@gmail.com>
2014-12-05 19:47:07 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,1 @@
-console.log('This would be the main JS file.');
+document.getElementById("nav01").innerHTML ="<ul id='menu'>" +"<li><a href='Index.html'>Home</a></li>" +"<li><a href='Customers.html'>Data</a></li>" +"<li><a href='About.html'>About</a></li>" +"</ul>";
