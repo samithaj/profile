19a3f6a7d38fcfc96fa02962eecc4a3da1eef111
Hasitha Prageeth <haztha@gmail.com>
2014-12-07 08:31:25 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,1 @@
-document.getElementById("nav01").innerHTML ="<ul id='menu'>" +"<li><a href='Index.html'>About Me</a></li>"
-+"<li><a href='education.html'>Education</a></li>"
-+"<li><a href='honors.html'>Honors & Awards</a></li>"
-+"<li><a href='experience.html'>Experience</a></li>"
-+"<li><a href='projects.html'>Projects</a></li>"
-+"<li><a href='skills.html'>Skills</a></li>"
-+"<li><a href='interests.html'>Interests</a></li>"
-+"</ul>";
+document.getElementById("nav01").innerHTML ="<ul id='menu'>" +"<li><a href='Index.html'>About Me</a></li>"+"<li><a href='education.html'>Education</a></li>"+"<li><a href='honors.html'>Honors & Awards</a></li>"+"<li><a href='experience.html'>Experience</a></li>"+"<li><a href='projects.html'>Projects</a></li>"+"<li><a href='skills.html'>Skills</a></li>"+"<li><a href='interests.html'>Interests</a></li>"+"</ul>";
